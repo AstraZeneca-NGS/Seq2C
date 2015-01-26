ee895389f42f941d0300d1e739c0f31ad314fd9e
vladsaveliev <vladsaveliev@me.com>
2015-01-27 00:43:45 +0300

    Absolute paths for seq2c scripts; The -z option for seq2cov.pl; Added .gitignore

--- seq2c.sh
+++ seq2c.sh
@@ -26,12 +26,12 @@ if [ -n "$SGE_OPT" ]; then
     a=(${i//\\t/}) 
     qsub $SGE_OPT -pe smp 1 -cwd -V -N seq2c_sam${COUNTER} -S /bin/bash ${DIR}/seq2cov_wrap.sh ${a[1]} ${a[0]} $BED $COUNTER
   done < $SAM2BAM
-  waitVardict.pl seq2c $COUNTER
+  perl ${DIR}/waitVardict.pl seq2c $COUNTER
   cat cov.txt.* > cov.txt
 else
-  cat $SAM2BAM | while read i; do a=(${i//\\t/}); seq2cov.pl -b ${a[1]} -N ${a[0]} $BED; done > cov.txt
+  cat $SAM2BAM | while read i; do a=(${i//\\t/}); perl ${DIR}/seq2cov.pl -z -b ${a[1]} -N ${a[0]} $BED; done > cov.txt
 fi
 bam2reads.pl $SAM2BAM > read_stats.txt
 
 #echo cov2lr.pl -a $CONS read_stats.txt cov.txt lr2gene.pl $OPT 
-cov2lr.pl -a $CONS read_stats.txt cov.txt | lr2gene.pl $OPT $SEQ2COPT > seq2c_results.txt
+perl ${DIR}/cov2lr.pl -a $CONS read_stats.txt cov.txt | perl ${DIR}/lr2gene.pl $OPT $SEQ2COPT > seq2c_results.txt
--- seq2cov_wrap.sh
+++ seq2cov_wrap.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 BAM=$1
 SAMPLE=$2
 BED=$3
 N=$4
-seq2cov.pl -b $BAM -N $SAMPLE $BED > cov.txt.${N}
+perl ${DIR}/seq2cov.pl -z -b $BAM -N $SAMPLE $BED > cov.txt.${N}
 touch seq2c.done.${N}
