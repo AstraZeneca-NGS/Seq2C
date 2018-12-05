56f8e7d4c78b835b11e680d435b1ce1269bf6172
chapmanb <chapmanb@50mail.com>
2018-12-05 12:11:19 -0500

    use Seq2C libraries for Stat::Basic dependency
    Stat::Basic is a custom dependency inside `libraries` not available in CPAN.
    This restores the previous behavior of getting it from the base install
    directory.
    
    Also swaps a couple of perl header calls to consistently use /usr/bin/env

--- lr2gene.pl
+++ lr2gene.pl
@@ -3,6 +3,9 @@
 # Normalize the coverage from targeted sequencing to CNV log2 ratio.  The algorithm assumes the medium 
 # is diploid, thus not suitable for homogeneous samples (e.g. parent-child).
 
+use FindBin;
+use lib "$FindBin::RealBin/libraries/";
+
 use Stat::Basic;
 use Statistics::TTest;
 use Getopt::Std;
