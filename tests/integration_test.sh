09dd4c97d86ab454868521315b44aa98aa5f22c2
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2019-05-29 12:34:08 +0000

    Updated code for integration testing

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -1,71 +1,99 @@
-#!/bin/bash
-# ***IMPORTANT***
-# Before run you have to set paths to the BAM files of GTL_16_5 and controls in samples/GTL_16_5_sample2bam.txt
+#!/bin/bash -eu
+set -o pipefail
+
+#-------------------------------------------------------------------------------
+# IMPORTANT: Before run you have to set paths to the BAM files of 
+#            GTL_16_5 and controls in samples/GTL_16_5_sample2bam.txt
+#-------------------------------------------------------------------------------
 
 OUTPUT="seq2c_results.txt"
+OUTPUT_DIR="/tmp/seq2c_test"
 
-#Run samples
-echo Run sample1 test without control sample
-SAMPLE2BAM="samples/Colo_sample1_hg19_chr20_sample2bam.txt"
-BED="bed/hg19_chr20_Colo_sample1.bed"
+SCRIPT_DIR=`cd "$(dirname $0)"; pwd -P`
+SEQ2C_PATH="$SCRIPT_DIR/.."
+TEST_DIR="$SCRIPT_DIR"
+echo "SCRIPT_DIR=$SCRIPT_DIR"
+echo "SEQ2C_PATH=$SEQ2C_PATH"
 
-seq2c.sh $SAMPLE2BAM $BED
-ACTUAL_SAMPLE1="seq2c_results_sample1.txt"
-mv $OUTPUT $ACTUAL_SAMPLE1
+# Add seq2c to path
+PATH="$PATH:$SEQ2C_PATH"
 
+#---
+# Compare results
+# Arguments: test_name expected_resutls actual_results
+#---
+function diff_results {
+	TEST_NAME="$1"
+	EXPECTED="$2"
+	RESULTS="$3"
 
-echo Run sample1 test with control sample
-CONTROLSAMPLE="Colo829.chr20.18.Sample1"
-SAMPLE2BAM="samples/Colo_sample1_hg19_chr20_sample2bam.txt"
-BED="bed/hg19_chr20_Colo_sample1.bed"
+	DIFF_FILE="$OUTPUT_DIR/test_$TEST_NAME.diff.txt"
+	echo "Test $TEST_NAME: Compare results, creating diff file '$DIFF_FILE'"
 
-seq2c.sh $SAMPLE2BAM $BED $CONTROLSAMPLE
-ACTUAL_SAMPLE1_CONTROL="seq2c_results_sample1_control.txt"
-mv $OUTPUT $ACTUAL_SAMPLE1_CONTROL
+	if diff <(sort $EXPECTED) <(sort $RESULTS) > $DIFF_FILE
+	then
+		echo "Test $TEST_NAME: OK"
+	else
+		echo "Test $TEST_NAME: ERROR"
+		exit 1;
+	fi
+}
 
-#Compare results
-echo Compare results of sample 1
-DIFF_FILE_SAMPLE1="diff_sample1.txt"
-EXPECTED_SAMPLE1="expected/expected_Colo_sample1.txt"
+#---
+# Test 1
+#---
+function test_1 {
+	echo "Test 1: Run sample1 test without control sample"
+	SAMPLE2BAM="samples/Colo_sample1_hg19_chr20_sample2bam.txt"
+	BED="bed/hg19_chr20_Colo_sample1.bed"
+	ACTUAL="$OUTPUT_DIR/test_1.seq2c_results_sample1.txt"
+	EXPECTED="expected/expected_Colo_sample1.txt"
 
-if diff <(sort $EXPECTED_SAMPLE1) <(sort $ACTUAL_SAMPLE1) > $DIFF_FILE_SAMPLE1
-then
-	echo "OK: no differences";
-else
-	echo "ERROR: files have differences!"
-	exit 1;
-fi
+	seq2c.sh $SAMPLE2BAM $BED
+	mv $OUTPUT $ACTUAL
+	diff_results 1 "$EXPECTED" "$ACTUAL"
+}
 
-echo Compare results of sample 1 with control sample
-DIFF_FILE_SAMPLE1_CONTROL="diff_sample1_control.txt"
-EXPECTED_SAMPLE1_CONTROL="expected/expected_Colo_sample1_control.txt"
+#---
+# Test 2
+#---
+function test_2 {
+	echo "Test 2: Run sample1 test with control sample"
+	CONTROL_SAMPLE="Colo829.chr20.18.Sample1"
+	SAMPLE2BAM="samples/Colo_sample1_hg19_chr20_sample2bam.txt"
+	BED="bed/hg19_chr20_Colo_sample1.bed"
+	ACTUAL="$OUTPUT_DIR/test_2.seq2c_results_sample1_control.txt"
+	EXPECTED="expected/expected_Colo_sample1_control.txt"
 
-if diff <(sort $EXPECTED_SAMPLE1_CONTROL) <(sort $ACTUAL_SAMPLE1_CONTROL) > $DIFF_FILE_SAMPLE1_CONTROL
-then
-	echo "OK: no differences";
-else
-	echo "ERROR: files have differences!"
-	exit 1;
-fi
+	seq2c.sh $SAMPLE2BAM $BED $CONTROL_SAMPLE
+	mv $OUTPUT $ACTUAL
+	diff_results 2 "$EXPECTED" "$ACTUAL"
+}
 
-echo Run GTL_16_5 test with control sample. Starts if previous small tests are ok.
-CONTROLSAMPLE="control1:control2:control3:control4:control5:control6:"
-SAMPLE2BAM="samples/GTL_16_5_sample2bam.txt"
-BED="bed/panel_az600_chr7.bed"
+#---
+# Test 3
+#---
+function test_3 {
+	echo "Test 3: Run GTL_16_5 test with control sample. Starts if previous small tests are ok."
+	CONTROL_SAMPLE="control1:control2:control3:control4:control5:control6:"
+	SAMPLE2BAM="samples/GTL_16_5_sample2bam.txt"
+	BED="bed/panel_az600_chr7.bed"
+	ACTUAL="$OUTPUT_DIR/test_3.seq2c_results_GTL_16_5.txt"
+	EXPECTED="expected/expected_GTL_16_5.txt"
 
-seq2c.sh $SAMPLE2BAM $BED $CONTROLSAMPLE
-ACTUAL_GTL_16_5_CONTROL="seq2c_results_GTL_16_5.txt"
-mv $OUTPUT $ACTUAL_GTL_16_5_CONTROL
+	seq2c.sh $SAMPLE2BAM $BED $CONTROL_SAMPLE
+	mv $OUTPUT $ACTUAL
+	diff_results 3 "$EXPECTED" "$ACTUAL"
+}
 
-#Compare results
-echo Compare results of GTL_16_5
-DIFF_FILE_GTL_16_5="diff_GTL_16_5.txt"
-EXPECTED_GTL_16_5="expected/expected_GTL_16_5.txt"
+#---
+# Main: Run all tests
+#---
 
-if diff <(sort $EXPECTED_GTL_16_5) <(sort $ACTUAL_GTL_16_5_CONTROL) > $DIFF_FILE_GTL_16_5
-then
-	echo "OK: no differences";
-else
-	echo "ERROR: files have differences!"
-	exit 1;
-fi
+cd "$TEST_DIR"
+mkdir "$OUTPUT_DIR" || true
+
+test_1
+test_2
+test_3
+
