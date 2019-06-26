8daa4ac47c452131d76b5bda211087e34b45c20c
PolinaBevad <polina_bevad@epam.com>
2019-06-26 15:31:36 +0300

    Added integration test for dfj example: top good and bad genes

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -40,6 +40,47 @@ function diff_results {
 }
 
 #---
+# Check for absence of bad genes and presence of top good genes with ratios in results
+# Arguments: test_name expected_bad_genes expected_good_genes actual_results
+#---
+function diff_genes {
+	TEST_NAME="$1"
+	EXPECTED_BAD_GENES="$2"
+	EXPECTED_TOP_GENES="$3"
+	RESULTS="$4"
+
+    # Create empty diff file
+	echo "Test $TEST_NAME: Creating diff file '$DIFF_FILE'"
+	DIFF_FILE="$OUTPUT_DIR/test_$TEST_NAME.diff.txt"
+	> $DIFF_FILE
+
+    # Check if there are no bad genes in results
+	echo "Test $TEST_NAME: Compare bad genes, append differences to '$DIFF_FILE'"
+    while read BAD_GENE; do
+        if grep -P "\t$BAD_GENE\t" $RESULTS > /dev/null; then
+            echo "Bad gene presence in results: $BAD_GENE" >> $DIFF_FILE
+        fi
+    done < $EXPECTED_BAD_GENES
+
+    # Check if there are all good top genes with ratios and lengths in results
+	echo "Test $TEST_NAME: Compare top good genes, append differences to '$DIFF_FILE'"
+    while read GOOD_GENE; do
+        if grep -P "$GOOD_GENE" $RESULTS > /dev/null; then
+            continue
+        else
+            echo "Good gene absence or another results: $GOOD_GENE" >> $DIFF_FILE
+        fi
+    done < $EXPECTED_TOP_GENES
+
+    if [ ! -s $DIFF_FILE ]; then
+        echo "Test $TEST_NAME: OK"
+    else
+        echo "Test $TEST_NAME: ERROR"
+        exit 1;
+    fi
+}
+
+#---
 # Test 1
 #---
 function test_1 {
@@ -49,7 +90,7 @@ function test_1 {
 	ACTUAL="$OUTPUT_DIR/test_1.seq2c_results_sample1.txt"
 	EXPECTED="$TEST_DIR/expected/expected_Colo_sample1.txt"
 
-	seq2c.sh $SAMPLE2BAM $BED
+	$SEQ2C_PATH/seq2c.sh $SAMPLE2BAM $BED
 	mv $OUTPUT $ACTUAL
 	diff_results 1 "$EXPECTED" "$ACTUAL"
 }
@@ -65,7 +106,7 @@ function test_2 {
 	ACTUAL="$OUTPUT_DIR/test_2.seq2c_results_sample1_control.txt"
 	EXPECTED="$TEST_DIR/expected/expected_Colo_sample1_control.txt"
 
-	seq2c.sh $SAMPLE2BAM $BED $CONTROL_SAMPLE
+	$SEQ2C_PATH/seq2c.sh $SAMPLE2BAM $BED $CONTROL_SAMPLE
 	mv $OUTPUT $ACTUAL
 	diff_results 2 "$EXPECTED" "$ACTUAL"
 }
@@ -81,18 +122,65 @@ function test_3 {
 	ACTUAL="$OUTPUT_DIR/test_3.seq2c_results_GTL_16_5.txt"
 	EXPECTED="$TEST_DIR/expected/expected_GTL_16_5.txt"
 
-	seq2c.sh $SAMPLE2BAM $BED $CONTROL_SAMPLE
+	$SEQ2C_PATH/seq2c.sh $SAMPLE2BAM $BED $CONTROL_SAMPLE
 	mv $OUTPUT $ACTUAL
 	diff_results 3 "$EXPECTED" "$ACTUAL"
 }
 
 #---
+# Test 4 (Test for checking bad, good genes on 2 samples from dfj example seq2c-coverage).
+# COVERAGE variable must be set to a path to /seq2c-coverage_2samples.tsv
+#---
+function test_4 {
+	echo "Test 4: Run dfj test for 2 samples 2-444961 and 2-444962"
+
+	READ_MAPPING="$TEST_DIR/coverage/seq2c-read_mapping.txt"
+    COVERAGE="$TEST_DIR/coverage/seq2c-coverage_2samples.tsv"
+
+	ACTUAL="$OUTPUT_DIR/test_4.seq2c_results_dfj_2samples.txt"
+	EXPECTED_BAD="$TEST_DIR/expected/bad_genes_2samples.txt"
+	EXPECTED_TOP="$TEST_DIR/expected/top_genes_2samples.txt"
+
+	echo "Test 4: Running cov2lr.pl script"
+    $SEQ2C_PATH/cov2lr.pl $READ_MAPPING $COVERAGE > cov2lr.results.txt
+    echo "Test 4: Running lr2gene.pl script"
+    $SEQ2C_PATH/lr2gene.pl cov2lr.results.txt > seq2c_results.txt
+
+	mv $OUTPUT $ACTUAL
+	diff_genes 4 "$EXPECTED_BAD" "$EXPECTED_TOP" "$ACTUAL"
+}
+
+#---
+# Test 5 (Test for checking bad, good genes on all samples from dfj example seq2c-coverage).
+# COVERAGE variable must be set to a path to /seq2c-coverage.tsv
+#---
+function test_5 {
+	echo "Test 5: Run dfj test for all samples"
+
+	READ_MAPPING="$TEST_DIR/coverage/seq2c-read_mapping.txt"
+    COVERAGE="$TEST_DIR/coverage/seq2c-coverage.tsv"
+
+	ACTUAL="$OUTPUT_DIR/test_5.seq2c_results_dfj_all_samples.txt"
+	EXPECTED_BAD="$TEST_DIR/expected/bad_genes_all.txt"
+	EXPECTED_TOP="$TEST_DIR/expected/top_genes_all.txt"
+
+	echo "Test 5: Running cov2lr.pl script"
+    $SEQ2C_PATH/cov2lr.pl $READ_MAPPING $COVERAGE > cov2lr.results.txt
+    echo "Test 5: Running lr2gene.pl script"
+    $SEQ2C_PATH/lr2gene.pl cov2lr.results.txt > seq2c_results.txt
+
+	mv $OUTPUT $ACTUAL
+	diff_genes 5 "$EXPECTED_BAD" "$EXPECTED_TOP" "$ACTUAL"
+}
+
+#---
 # Main: Run all tests
 #---
-
 mkdir "$OUTPUT_DIR" || true
 
 test_1
 test_2
 test_3
+test_4
+test_5
 
